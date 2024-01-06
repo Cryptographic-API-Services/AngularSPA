40f6f39734088bf44b67636f3158b080cb745ad1
Mike Mulchrone <mtmulch0191@outlook.com>
2024-01-06 14:39:58 -0500

    #2 basic get benchmark based off of dropdown options

--- src/app/modules/user-home/user-benchmarks/user-benchmarks.component.ts
+++ src/app/modules/user-home/user-benchmarks/user-benchmarks.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { HttpService } from 'src/app/services/http.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-user-benchmarks',
-  standalone: true,
-  imports: [],
   templateUrl: './user-benchmarks.component.html',
   styleUrl: './user-benchmarks.component.css'
 })
-export class UserBenchmarksComponent {
+export class UserBenchmarksComponent implements AfterViewInit {
 
+  @ViewChild('benchmarkDateSelect')
+  benchmarkDate: ElementRef;
+
+  constructor(private httpService: HttpService) {
+
+  }
+
+  ngAfterViewInit(): void {
+    this.getInitialChart(this.benchmarkDate.nativeElement.value);
+  }
+
+  public handleDateChangeEvent(): void {
+    this.getInitialChart(this.benchmarkDate.nativeElement.value);
+  }
+
+  private getInitialChart(daysAgo: number): void {
+    const url = environment.apiUrl + "BenchmarkSDKMethod/GetUserBenchmarksByDays" + `?daysAgo=${daysAgo}`
+    this.httpService.getAuthenticated(url).subscribe((response) => {
+      console.log(response);
+    });
+  }
 }
--- src/app/modules/user-home/user-home.module.ts
+++ src/app/modules/user-home/user-home.module.ts
@@ -13,6 +13,7 @@ import { SharedModule } from 'src/app/shared/modules/shared/shared.module';
 import { ProductsSelectionComponent } from './products/products-selection.component';
 import { EditBillingInformationComponent } from './edit-billing-information/edit-billing-information.component';
 import { UserSettingsComponent } from './user-settings/user-settings.component';
+import { UserBenchmarksComponent } from './user-benchmarks/user-benchmarks.component';
 
 
 @NgModule({
@@ -25,7 +26,8 @@ import { UserSettingsComponent } from './user-settings/user-settings.component';
     ApiKeyComponent,
     ProductsSelectionComponent,
     EditBillingInformationComponent,
-    UserSettingsComponent
+    UserSettingsComponent,
+    UserBenchmarksComponent
   ],
   imports: [
     CommonModule,
