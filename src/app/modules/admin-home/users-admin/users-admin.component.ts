b01c92e87207268df83984bf62abc3452553b197
WingZer0o <mtmulch0191@outlook.com>
2024-03-16 09:36:53 -0400

    #18 user admin creation screen

---
+++ src/app/modules/admin-home/users-admin/users-admin.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from 'src/app/services/http.service';
+
+@Component({
+  selector: 'app-users-admin',
+  templateUrl: './users-admin.component.html',
+  styleUrl: './users-admin.component.css'
+})
+export class UsersAdminComponent implements OnInit {
+
+  constructor(private httpService: HttpService) {
+
+  }
+
+  ngOnInit(): void {
+    this.getUsersPage();
+  }
+
+}
