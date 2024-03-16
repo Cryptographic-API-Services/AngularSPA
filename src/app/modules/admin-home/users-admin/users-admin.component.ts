29092747384fff00f522331daf8f7d238b2db260
WingZer0o <mtmulch0191@outlook.com>
2024-03-16 17:06:26 -0400

    #18 user admin component

--- src/app/modules/admin-home/users-admin/users-admin.component.ts
+++ src/app/modules/admin-home/users-admin/users-admin.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpService } from 'src/app/services/http.service';
+import { environment } from 'src/environments/environment';
+import { UserTableItem } from './types/user-table-item';
 
 @Component({
   selector: 'app-users-admin',
@@ -7,6 +9,11 @@ import { HttpService } from 'src/app/services/http.service';
   styleUrl: './users-admin.component.css'
 })
 export class UsersAdminComponent implements OnInit {
+
+  private pageIndex: number = 0;
+  private pageSize: number = 5;
+  public userList: UserTableItem[];
+  public count: number = 0;
 
   constructor(private httpService: HttpService) {
 
@@ -17,6 +24,56 @@ export class UsersAdminComponent implements OnInit {
   }
 
   private getUsersPage(): void {
+    this.httpService.getAuthenticated(environment.apiUrl + `/UserAdmin/GetUsers?PageSkip=${this.pageIndex}&PageSize=${this.pageSize}`).subscribe((response: any) => {
+      this.userList = response.userTableItems;
+      this.count = response.count;
+    });
+  }
 
+  public makeUserInactive(user: UserTableItem): void {
+    let body = {
+      UserId: user.userId,
+      IsActive: false
+    };
+    this.httpService.putAuthenticated(environment.apiUrl + "/UserAdmin/UserActivationStatus", body).subscribe(() => {
+      user.isActive = false;
+    });
+  }
+
+  public makeUserActive(user: UserTableItem): void {
+    let body = {
+      UserId: user.userId,
+      IsActive: true
+    };
+    this.httpService.putAuthenticated(environment.apiUrl + "/UserAdmin/UserActivationStatus", body).subscribe(() => {
+      user.isActive = true;
+    });
+  }
+
+  public makeUserRegularUser(user: UserTableItem): void {
+    let body = {
+      UserId: user.userId,
+      IsAdmin: false
+    };
+    this.httpService.putAuthenticated(environment.apiUrl + "/UserAdmin/UserAdminStatus", body).subscribe(() => {
+      user.isAdmin = false;
+    });
+  }
+
+  public makeUserAdmin(user: UserTableItem): void {
+    let body = {
+      UserId: user.userId,
+      IsAdmin: true
+    };
+    this.httpService.putAuthenticated(environment.apiUrl + "/UserAdmin/UserAdminStatus", body).subscribe(() => {
+      user.isAdmin = true;
+    });
+  }
+
+  public deleteUser(user: UserTableItem): void {
+    let body = { UserId: user.userId };
+    this.httpService.deleteAuthenticated(environment.apiUrl + "/UserAdmin/UserDelete", body).subscribe(() => {
+      this.userList.splice(this.userList.findIndex(x => x.userId === user.userId), 1);
+    });
   }
 }
