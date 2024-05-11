594accbd0780b02d9108289ab9b72bedfb11ccc7
WingZer0o <mtmulch0191@outlook.com>
2024-05-11 16:38:07 -0400

    radio button for api key

--- src/app/modules/user-home/api-key/api-key.component.ts
+++ src/app/modules/user-home/api-key/api-key.component.ts
@@ -1,29 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpService } from 'src/app/services/http.service';
-import { environment } from 'src/environments/environment';
+import { Component, OnInit } from "@angular/core";
+import { HttpService } from "src/app/services/http.service";
+import { environment } from "src/environments/environment";
 
 @Component({
-  selector: 'app-api-key',
-  templateUrl: './api-key.component.html',
-  styleUrls: ['./api-key.component.css']
+  selector: "app-api-key",
+  templateUrl: "./api-key.component.html",
+  styleUrls: ["./api-key.component.css"],
 })
 export class ApiKeyComponent implements OnInit {
   public apiKey: string = "";
   public developmentKey: string = "";
 
-  constructor(private httpService: HttpService) { }
+  public isProductionKey: boolean = true;
+  public isDevelopmentKEY: boolean = false;
+
+  constructor(private httpService: HttpService) {}
 
   ngOnInit(): void {
     this.getApiKey();
   }
 
-
   private getApiKey(): void {
-    this.httpService.getAuthenticated(environment.apiUrl + "UserLogin/GetApiKey").subscribe((response: any) => {
+    this.httpService.getAuthenticated(
+      environment.apiUrl + "UserLogin/GetApiKey",
+    ).subscribe((response: any) => {
       this.apiKey = response.apiKey;
       this.developmentKey = response.developmentKey;
     }, (error) => {
-
     });
+  }
+
+  public formCheckChanged(event: any) {
+    this.isProductionKey = !this.isProductionKey;
+    this.isDevelopmentKEY = !this.isDevelopmentKEY;
   }
 }
