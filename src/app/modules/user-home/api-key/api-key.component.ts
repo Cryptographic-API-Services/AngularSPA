7026ce5098ed177c572f0a3bc45a54af2a615840
Mike Mulchrone <mtmulch0191@outlook.com>
2024-09-19 21:21:15 -0400

    signals for api keys

--- src/app/modules/user-home/api-key/api-key.component.ts
+++ src/app/modules/user-home/api-key/api-key.component.ts
@@ -1,37 +1,25 @@
 import { Component, OnInit } from "@angular/core";
 import { HttpService } from "src/app/services/http.service";
 import { environment } from "src/environments/environment";
+import { ApiKeyService } from "./api-key.service";
 
 @Component({
   selector: "app-api-key",
   templateUrl: "./api-key.component.html",
   styleUrls: ["./api-key.component.css"],
+  providers: [ApiKeyService]
 })
 export class ApiKeyComponent implements OnInit {
-  public apiKey: string = "";
-  public developmentKey: string = "";
 
-  public isProductionKey: boolean = true;
-  public isDevelopmentKEY: boolean = false;
-
-  constructor(private httpService: HttpService) {}
+  constructor(
+    public apiKeyService: ApiKeyService
+  ) {}
 
   ngOnInit(): void {
-    this.getApiKey();
-  }
-
-  private getApiKey(): void {
-    this.httpService.getAuthenticated(
-      environment.apiUrl + "UserLogin/GetApiKey",
-    ).subscribe((response: any) => {
-      this.apiKey = response.apiKey;
-      this.developmentKey = response.developmentKey;
-    }, (error) => {
-    });
   }
 
   public formCheckChanged(event: any) {
-    this.isProductionKey = !this.isProductionKey;
-    this.isDevelopmentKEY = !this.isDevelopmentKEY;
+    this.apiKeyService.state.isProductionKey$.set(!this.apiKeyService.state.isProductionKey$());
+    this.apiKeyService.state.isDevelopmentKEY$.set(!this.apiKeyService.state.isDevelopmentKEY$());
   }
 }
---
+++ src/app/modules/user-home/api-key/api-key.service.ts
@@ -0,0 +1,29 @@
+import { Injectable, signal } from '@angular/core';
+import { HttpService } from 'src/app/services/http.service';
+import { environment } from 'src/environments/environment';
+
+@Injectable()
+export class ApiKeyService {
+
+  public state = {
+    apiKey$: signal<string>(""),
+    developmentKey$: signal<string>(""),
+    isProductionKey$: signal<boolean>(true),
+    isDevelopmentKEY$: signal<boolean>(false),
+    testing: signal<string[]>([]),
+  };
+
+  constructor(private httpService: HttpService) {
+    this.getApiKey();
+   }
+
+  private getApiKey(): void {
+    this.httpService.getAuthenticated(
+      environment.apiUrl + "UserLogin/GetApiKey",
+    ).subscribe((response: any) => {
+      this.state.apiKey$.set(response.apiKey);
+      this.state.developmentKey$.set(response.developmentKey);
+    }, (error) => {
+    });
+  }
+}
