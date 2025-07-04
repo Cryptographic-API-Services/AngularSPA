8666d41697a7bbc2bcc31f50cc7ef34854288441
Mike Mulchrone <mikemulchrone987@gmail.com>
2025-07-04 00:29:12 -0400

    removing chart js benchmark component

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -3,14 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home/home.component';
-import { BenchmarkMethodChartComponent } from './benchmark-method-chart/benchmark-method-chart.component';
 import { NgChartsModule } from 'ng2-charts';
 
 
 @NgModule({
   declarations: [
-    HomeComponent,
-    BenchmarkMethodChartComponent,
+    HomeComponent
   ],
   imports: [
     CommonModule,
